738b69b9c77ad5a1b285678f9d9b23d045acd74e
Henrique <henrique.scastro12@gmail.com>
2025-11-08 15:34:37 -0300

    docker local

--- cassandra.js
+++ cassandra.js
@@ -1,7 +1,7 @@
 const cassandra = require("cassandra-driver");
 
 const client = new cassandra.Client({
-  contactPoints: ["<IP_DO_SEU_CONTAINER>"], // <-- COLOQUE O IP DO SEU CONTAINER GERADO NO PLAYGROUND AQUI!
+  contactPoints: ['cassandra'], // <-- COLOQUE O IP DO SEU CONTAINER GERADO NO PLAYGROUND AQUI!
   localDataCenter: "datacenter1",
   keyspace: "avaliacao_filmes",
 });
